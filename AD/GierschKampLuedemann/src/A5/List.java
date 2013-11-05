e66d45e17a57f4ce9d6498374d7bde4f0a821d20
unknown <abl128@.(none)>
2013-11-05 18:12:30 +0100

    Ad 4 +5

---
+++ AD/GierschKampLuedemann/src/A5/List.java
@@ -0,0 +1,42 @@
+package A5;
+
+import A1.Liste;
+
+
+public class List  {
+	Object head = null;
+	List tail = null;
+	
+	public List() {
+	}
+	
+	private List(Object head, List tail) {
+		this.head = head;
+		this.tail = tail;
+	}
+	
+	public Object head() {
+		return head;
+	}
+	
+	public List tail() {
+		return tail;
+	}
+	
+	public void head(Object n) {
+		this.tail = new List(this.head, this.tail);
+		this.head = n;
+	}
+	
+	public boolean insert(Object n, int i) {
+		if(i == 1) {
+			head(n);
+			return true;
+		} else if (tail == null) {
+			return false;
+		} else {
+			tail.insert(n,i-1);
+			return true;
+		}
+	}
+}
---
+++ AD/GierschKampLuedemann/src/A5/main.java
@@ -0,0 +1,62 @@
+package A5;
+
+import A1.Liste;
+import A1.ListeImpl;
+import A1.Messung;
+
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Messung m1 = new Messung();
+
+//		for (int t = 0; t < 100; t++) {
+//			Liste list1 = new ListeImpl();
+//			List list2 = new List();
+//			
+//			for (int i = 1; i <= 100; i++) {
+//				int n = (int) (Math.rint(Math.random() * (i-1)));
+//				list1.insert(i, n);
+//				list2.insert(n, i);
+//			}
+//
+//		}
+//		Liste list1 = new ListeImpl();
+//		List list2 = new List();
+//		
+//		for (int i = 1; i <= 100; i++) {
+//			int n = (int) (Math.rint(Math.random() * (i-1)));
+//			list1.insert(i, n);
+//			list2.insert(n, i);
+//			System.out.println("I: "+i +" N: "+ n);
+//		}
+//		System.out.println(list1.length());
+//		for( int i = 1; i < list1.length(); i++) {
+//			System.out.print(list1.head() + " ");
+//		}
+//		System.out.println("");
+//		while(list2.head() != null) {
+//			System.out.print((int) list2.head() + " ");
+//			list2 = list2.tail();
+//		}
+//
+		Liste list1 = new ListeImpl();
+		
+		for (int i = 1; i <= 3; i++) {
+			int n = (int) (Math.rint(Math.random() * (i-1)));
+			list1.insert(i, n);
+			System.out.println("I: "+i +" N: "+ n);
+			System.out.print("N2: " + list1.top() + " ");
+		}
+		
+//		System.out.println(list1.length());
+//		for( int i = 1; i < list1.length(); i++) {
+//			System.out.print(list1.head() + " ");
+//		}
+	}
+
+}
