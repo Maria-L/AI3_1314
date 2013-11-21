2defa850ebad0fc046f291ae5c79f4696fc0f31e
unknown <abl128@.INFORMATIK>
2013-11-21 10:01:00 +0100

    GKAPR3 fortfuehrung

--- GKA/GKA/src/graphReader/main.java
+++ GKA/GKA/src/graphReader/main.java
@@ -163,5 +163,56 @@ public class main {
 
 		return graph;
 	}
+	
+	
+	public static Graph graphReaderArg(BufferedReader br, String arg) throws IOException {
+		String line = null;
+		Graph graph = new GraphImpl();
+
+		if (br.readLine().startsWith("#g")) {	//Prüfen ob der Graph gerichtet ist oder nicht
+			graph.setDirected(true);
+		} else {
+			graph.setDirected(false);
+		}
+
+		while ((line = br.readLine()) != null) {								//Ausführen, bis es keine Zeilen mehr gibt
+			String[] parts = line.split(",");									//Linie nach Komata splitten
+			int v1 = -1;
+			int v2 = -1;
+			HashMap<String, Integer> names = new HashMap<String, Integer>();	//Map von Namen auf ID
+
+			for (int v : graph.getVertexes()) {									//Namen aktualisieren
+				names.put(graph.getStrV(v, "name"), v);
+			}
+
+			if (names.keySet().contains(parts[0])) {							//Prüfen ob der erste Knoten schon im Graphen ist
+				v1 = names.get(parts[0]);										//Wenn ja: ID des Knoten raussuchen
+			} else {
+				v1 = graph.addVertex(parts[0]);									//Wenn nein: neuen Knoten in dem Graphen ablegen
+			}
+
+			if (names.keySet().contains(parts[1])) {							//Wie mit Knoten 1
+				v2 = names.get(parts[1]);
+			} else {
+				v2 = graph.addVertex(parts[1]);
+			}
+
+			for (int i = 2; i < parts.length; i++) {							//Da die restlichen Stellen der Zeile Kantenattribute 
+				Id id = new Id(0);												//sind dementsprechende Kanten zufügen
+				int e1;
+				String name = arg;										        //arg zuweisen
+
+				if (graph.directed()) {	
+					e1 = graph.addEdgeD(v1, v2);								//Kante hinzufügen
+				} else {
+					e1 = graph.addEdgeU(v1, v2);
+				}
+
+				graph.setValE(e1, name, Integer.parseInt(parts[i]));			//Attribut an diese Kante setzen
+			}
+		}
+
+		return graph;
+	}
 
 }
