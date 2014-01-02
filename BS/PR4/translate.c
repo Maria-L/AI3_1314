ed15dc5cd735a49738c9256ce7c49d7fad8b6f7b
user <user@linux-0n80.site>
2014-01-02 23:04:41 +0100

    BSPR4 weiter weiter ins Verderben

--- BS/PR4/translate.c
+++ BS/PR4/translate.c
@@ -16,3 +16,100 @@
 
 MODULE_AUTHOR("Steffen Giersch, Maria Janna Martina Luedemann");
 MODULE_LICENSE("Dual BSD/GPL");
+
+
+static char *dev_name = "translate";         //Name des Geraets
+static int count_of_devices = COUNT_OF_DEVS; //Anzahl der Sub-Devices
+
+int translate_bufsize = TRANSLATE_BUFSIZE;   //Groesse des Buffers
+
+char *translate_subst_def = TRANSLATE_SUBST; //Fixe Translate-Tabelle
+char *translate_subst = TRANSLATE_SUBST;     //Translate-Tabelle, die vom Nutzer vorgegeben werden kann
+
+int dev_minor = 0; //Minor-Device-Number, muss noch vergeben werden
+int dev_major = 0; //Major-Device-Number, muss noch vergeben werden
+
+//Gegebenenfalls ersetzen der Parameter durch Nutzereingaben
+module_param(translate_bufsize, int, 0);  
+module_param(translate_subst, charp, 0);
+
+
+struct translate_dev *translate_devices;  //Pointer auf alle Translate-devices
+
+//"Verlinken" der Standart-Operations-Namen auf die passenden Translate-Operationen
+struct file_operations fops = {
+  .owner = THIS_MODULE,
+  .read = translate_read,
+  .write = translate_write,
+  .open = translate_open,
+  .release = translate_close
+};
+
+//########## Zugriffsfunktionen ##########
+ssize_t translate_read(struct file *filp, char __user * buf, size_t count, loff_t * f_pos) {
+  int err;                   //Error-Wert
+  int minor;                 //Minor-Device-Number
+  int i;                     //Zaehler-Variable
+  struct translate_dev *dev; //Translate-Informationen
+  
+  dev = filp->private_data;  //Nehme Translate-Informationen von der Benutzereingabe
+  minor = dev->minor_number; //Speichere die Minor-Device-Number
+  
+  err = down_interruptible(&dev->sem);  //Warte auf den Semaphoren
+  if(err) {                             //Wenn der Prozess per Signal geweckt wurde
+    return -ERESTARTSYS;                //  Gib ERESTARTSYS zurueck
+  }
+  
+  if(dev->fillcount <= ZEROBUFFER) {                                //Wenn der Buffer leer ist
+    printk(KERN_ALERT "Empty buffer, waiting for elements\n");
+
+    up(&dev->sem);                                                  //  Gib den Semaphoren frei
+    err = wait_event_interruptible(dev->queue, dev->fillcount > 0); //  Reihe dich in die Warteschlange ein, bis die Bedingung erfuellt ist
+    if(err) {                                                       //  Wenn der Prozess per Signal geweckt wurde
+      return -ERESTARTSYS;                                          //    Gib ERESTARTSYS zurueck
+    }
+    err = down_interruptible(&dev->sem);                            //  Warte auf den Semaphoren
+    if(err) {                                                       //  Wenn der prozess per Signal geweckt wurde
+      return -ERESTARTSYS;                                          //    Gib ERESTARTSYS zurueck
+    }
+  }
+  
+  if(dev->fillcount == 0) {  //FRAGWUERDIG?!?
+    return 0;
+  }
+  
+  if(dev->wp > dev->rp) {                             //Wenn der Write-Pointer hinter dem Read-Pointer steht
+    count = min(count, (size_t) (dev->wp - dev->rp)); //  Setze count auf das minimum von count und den uebrigen Elementen vom wp bis zum rp
+  } else {                                            //Sonst
+    count = min(count, (size_t) (dev->end - dev->rp));//  Setze count auf das minimum von count und den uebrigen Elementen bis zum Ende des Buffers
+  }
+  
+  if(minor == ONE) {                   //Wenn Decodiert werden muss
+    for(i = 0; i < count; i++) {       //  Decodiere und schreibe count mal in den User-Speicher
+      buf[i] = decode_char(dev->rp[i]);
+    }
+  } else {                             //Wenn nicht decodiert werden muss
+    err = copy_to_user(buf, dev->rp, count);//Kopiere die Ergebnisse in den User-Speicher
+    if(err) {                          //  Wenn dabei ein Fehler passiert ist
+      up(&dev->sem);                   //    Gib den Semaphoren wieder frei           #### FRAGWUERDIG ####
+      return -EFAULT;                  //    Gebe EFAULT zurueck
+    }
+  }
+  
+  dev->rp += count;        //Setze den Read-Pointer weiter
+  if(dev->rp == dev->end) {   //Wenn der Read-Pointer am Ende des Buffers angekommen ist
+    dev->rp = dev->buffer; //  Setze den Read-Pointer auf den Anfang des Buffers
+  }
+  
+  dev->fillcount -= count; //Dekrementiere den Fill-Count
+  
+  printk(KERN_ALERT "Translate: Read %li Chars from translate%d\n", (long) count, minor);
+  up(&dev->sem);           //Gib den Semaphoren wieder frei
+  wake_up(&dev->queue);    //Wecke die Prozesse in der Warteschlange
+  
+  return count;
+}
+
+
+
+//########## Hilfsfunktionen ##########
--- BS/PR4/translate.h
+++ BS/PR4/translate.h
@@ -26,31 +26,50 @@
 #define ALPHABET_LENGTH ('Z'-'A'+1)
 #endif
 
-#ifdef TRANSLATE_SUBST_LENGTH
+#ifndef TRANSLATE_SUBST_LENGTH
 #define TRANSLATE_SUBST_LENGTH (ALPHABET_LENGTH * 2)
 #endif
 
+#ifndef ZEROBUFFER
+#define ZEROBUFFER 0
+#endif
+
+#ifndef ZERO
+#define ZERO 0
+#endif
+
+#ifndef ONE
+#define ONE 1
+#endif
+
+#ifndef DYNAMIC_MAJOR
+#define DYNAMIC_MAJOR 0
+#endif
+
+// Geraete-Struct fuer translate
 struct translate_dev {
-  char *buffer, *end;
-  int buffersize, fillcount;
-  char *rp, *wp;
-  int nreaders, nwriters;
-  int minor_number;
-  struct semaphore sem;
-  struct cdev cdev;
-  wait_queue_head_t queue;
+  char *buffer, *end;        //Buffer-Anfangs und End-adresse
+  int buffersize, fillcount; //Groesse des Buffers und die Anzahl der beschriebenen Stellen im Buffer
+  char *rp, *wp;             //Position des Read und des Write-Pointers
+  int nreaders, nwriters;    //Anzahl der lesenden und schreibenden Prozesse
+  int minor_number;          //Minor-Number des Geraetes: 0 -> Encode | 1 -> Decode
+  struct semaphore sem;      //Semaphore um gleichzeitige Zugriffe zu vermeiden
+  struct cdev cdev;          //Von der Inode-Struktur benoetigtes Element zur Repraesentation von Char-Devices
+  wait_queue_head_t queue;   //Warteschlange fuer Prozesse
 };
 
-int init_module(void);
-void cleanup_module(void);
-int translate_open(struct inode *inode, struct file *filp);
-int translate_close(struct inode *inode, struct file *filp);
-ssize_t translate_read(struct file *filp, char __user * buf, size_t count, loff_t * f_pos);
-ssize_t translate_write(struct file *filp, const char __user * buf, size_t count, loff_t * f_pos);
+//########## Zugriffsfunktionen ##########
+int init_module(void);      //Methode zum Initialisieren des Moduls
+void cleanup_module(void);  //Methode zum Freigeben des Moduls
+int translate_open(struct inode *inode, struct file *filp);  //Gewaehrt Zugriff auf ein Translate-Device
+int translate_close(struct inode *inode, struct file *filp); //Loescht den Zugriff auf das Translate-Device wieder
+ssize_t translate_read(struct file *filp, char __user * buf, size_t count, loff_t * f_pos);        //Liest aus dem Buffer des Geraetes
+ssize_t translate_write(struct file *filp, const char __user * buf, size_t count, loff_t * f_pos); //Schreibt in den Buffer des Geraetes
 
-char encode_char(char inputChar);
-char decode_char(char inputChar);
-int indexOf(char inputChar);
-void printDevice(struct translate_dev *dev);
+//########## Hilfsfunktionen ##########
+char encode_char(char inputChar);           //Verschluesselt den eingegebenen char nach der Kodierungstabelle
+char decode_char(char inputChar);           //Entschluesselt den eingegebenen char nach der Kodierungstabelle
+int indexOf(char inputChar);                //Sucht den Index des eingegebenen char im Buffer
+void printDevice(struct translate_dev *dev);//Schreibt den aktuellen Zustand des Geraets in das Kernellog
 
 #endif
