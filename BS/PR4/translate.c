33e311146d625e2c92c7b126617b0664eb2fb67c
user <user@linux-0n80.site>
2014-01-03 16:40:40 +0100

    BSPR4 auf in die Hoelle und zurueck

--- BS/PR4/translate.c
+++ BS/PR4/translate.c
@@ -55,13 +55,15 @@ ssize_t translate_read(struct file *filp, char __user * buf, size_t count, loff_
   dev = filp->private_data;  //Nehme Translate-Informationen von der Benutzereingabe
   minor = dev->minor_number; //Speichere die Minor-Device-Number
   
+  printk(KERN_ALERT "Translate: %s starting to read\n", buf);
+  
   err = down_interruptible(&dev->sem);  //Warte auf den Semaphoren
   if(err) {                             //Wenn der Prozess per Signal geweckt wurde
     return -ERESTARTSYS;                //  Gib ERESTARTSYS zurueck
   }
   
   if(dev->fillcount <= ZEROBUFFER) {                                //Wenn der Buffer leer ist
-    printk(KERN_ALERT "Empty buffer, waiting for elements\n");
+    printk(KERN_ALERT "Translate: Empty buffer, waiting for elements\n");
 
     up(&dev->sem);                                                  //  Gib den Semaphoren frei
     err = wait_event_interruptible(dev->queue, dev->fillcount > 0); //  Reihe dich in die Warteschlange ein, bis die Bedingung erfuellt ist
@@ -110,6 +112,66 @@ ssize_t translate_read(struct file *filp, char __user * buf, size_t count, loff_
   return count;
 }
 
+
+ssize_t translate_write(struct file *filp, const char __user * buf, size_t count, loff_t *f_pos) {
+  int err;                    //Error-Wert
+  int minor;                  //Minor-Device-Number
+  int i;                      //Zaehler-Variable
+  struct translate_dev *dev;  //Translate-Informationen
+  
+  dev = filp->private_data;   //Nehme Translate-Informationen von der Benutzereingabe
+  minor = dev->minor_number;  //Speichere die Minor-Device-Number
+  
+  printk(KERN_ALERT "Translate: %s starting to write\n" buf);
+  
+  err = down_interruptible(&dev->sem);  //Warte auf den Semaphoren
+  if(err) {                             //Wenn der Prozess per Signal geweckt wurde
+    return -ERESTARTSYS;                //  Gib ERESTARTSYS zurueck
+  }
+  
+  if(dev->fillcount >= translate_bufsize) {                                       //Wenn der Buffer voll ist
+    up(&dev->sem);                                                                //  gib den Semaphoren wieder frei
+    
+    err = wait_event_interruptible(dev->queue, dev->fillcount < translate_bufsize);// Reihe dich in die Warteschlange ein
+    if(err) {                                                                     //  Wenn der Prozess per Signal geweckt wurde
+      return -ERESTARTSYS;                                                        //    Gib ERESTARTSYS zurueck
+    }
+    
+    err = down_interruptible(&dev->sem);                                          //  Warte auf den Semaphoren
+    if(err) {                                                                     //  Wenn der Prozess per Signal geweckt wurde
+      return -ERESTARTSYS;                                                        //    Gib ERESTARTSYS zurueck
+    }
+  }
+  
+  count = min(count, (size_t) (translate_bufsize - dev->fillcount)); //Setze Count auf das Minimum von Count und den Uebrigen Buffer-Elementen
+  if(dev->wp >= dev->rp) {                                           //Wenn der Readpointer einen Wrap-Around hatte
+    count = min(count, (size_t) (dev->end - dev->wp));               //  Setze Count auf das Minimum von Count und den Elementen bis zum Ende der Liste
+  } else {                                                           //Sonst
+    count = min(count, (size_t) (dev->rp - dev->wp - 1));            //  Setze Count auf das Minimum von Count und den Elementen bis zum Read-Pointer
+  }
+  
+  if(minor == ONE) {                           //Wenn ########################## Weiter kommentieren
+    err = copy_from_user(dev->wp, buf, count);
+    if(err) {
+      up(&dev->queue);
+      return -EFAULT;
+    }
+  } else {
+    for(i = 0; i < count; i++) {
+      dev->wp[i] = encode_char(buf[i]);
+    }
+  }
+  
+  dev->wp += count;
+  if(dev->wp == dev->end) {
+    dev->wp = dev->buffer;
+  }
+  
+  dev->fillcount += count
+  wakt_up(&dev->queue);
+  up(&dev->sem);
+  return count;
+}
 
 
 //########## Hilfsfunktionen ##########
