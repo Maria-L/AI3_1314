38a11fd0cea7b50d88ac5ba857c0dbcfedd9657c
unknown <astreija@googlemail.com>
2014-01-06 15:56:24 +0100

    GKA Protokoll

--- SE/SE1/src/PR3/IZimmerkomponenteServices.java
+++ SE/SE1/src/PR3/IZimmerkomponenteServices.java
@@ -5,12 +5,7 @@ import java.util.*;
 
 public interface IZimmerkomponenteServices {
 	
-	/**Abfrage:
-	 * Kann jederzeit aufgerufen werden
-	 * Gibt zu dem eingegebenen Zimmer die dazu passende Art des Zimmers zurück
-	 * @param zimmer ZimmerID des abzufragenden Zimmers
-	 * @return ID der Zimmerart
-	 * @throws IllegalArgumentException Wenn das Zimmer nicht existiert
-	 */
+	
+	
 	Integer getZimmerArt(Integer zimmer) throws IllegalArgumentException;
 }
