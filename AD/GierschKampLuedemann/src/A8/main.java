d6ac21916d7faf9bf5876bcb46c81d3efc497988
Birger <Birger.Kamp@googlemail.com>
2014-01-08 20:07:16 +0100

    WAWAWAWAWAWAHH

--- AD/GierschKampLuedemann/src/A8/main.java
+++ AD/GierschKampLuedemann/src/A8/main.java
@@ -7,12 +7,27 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		IList list1 = new MyList();
-		list1 = list1.random(10000);
+		/* AUFGABE 8.5 */
 		
-		System.out.println("Unsortiert: " + list1);
-		System.out.println("Sortiert: " + list1.mergeSort());
-		System.out.println("Ist sortiert? " + list1.isIncreasingMonoton());
+		IList list1 = new MyList();
+		IList list2 = new MyList();
+		list1 = list1.sortIncreasingMonoton(10000);
+		list2 = list2.sortIncreasingMonoton(10000);
+		
+		list1.merge(list2);
+		
+		System.out.println("Sind die beiden Listen nun sortiert gemerged worden? " + list1.isIncreasingMonoton());
+		
+		
+		/* AUFGABE 8.7 */
+//		IList list1 = new MyList();
+//		list1 = list1.random(10000);
+//		
+//		System.out.println("Unsortiert: " + list1);
+//		System.out.println("Sortiert: " + list1.mergeSort());
+//		System.out.println("Ist sortiert? " + list1.isIncreasingMonoton());
+		
+		
 	}
 
 }
